538d8bae79a565e58056b604df23dcc0fb14f506
Abdurrahman <abdoarafh54@gmail.com>
2022-10-08 19:02:51 +0200

    added: more use of path buf to support more platforms

--- src/main.rs
+++ src/main.rs
@@ -44,21 +44,29 @@ fn main() {
         for j in 0..slices_count.1 {
             let subimg = imageops::crop(&mut image, i * slice_w, j * slice_h, slice_w, slice_h);
 
-            let file_path = args.file_path.to_string_lossy();
+            // let file_path = args.file_path.to_string_lossy();
             let mut output_folder = match args.output_folder {
                 Some(ref path) => path.to_owned(),
                 None => path::PathBuf::from("output/"),
             };
-            output_folder.push(
-                &file_path[find_last(&file_path, '/').unwrap_or(0)
-                    ..file_path.find(".").unwrap_or(file_path.len())],
-            );
+            let file_name = args
+                .file_path
+                .file_name()
+                .unwrap()
+                .to_string_lossy()
+                .to_string();
+            output_folder.push(&file_name[0..file_name.find('.').unwrap()]);
             fs::create_dir_all(&output_folder).unwrap();
 
-            let file_extension = &file_path[file_path.find(".").unwrap_or(0) + 1..];
+            let file_extension = &args.file_path.extension().unwrap();
 
             let mut output_path = output_folder;
-            output_path.push(format!("{},{}.{}", i + 1, j + 1, file_extension));
+            output_path.push(format!(
+                "{},{}.{}",
+                i + 1,
+                j + 1,
+                file_extension.to_string_lossy()
+            ));
 
             subimg.to_image().save(&output_path).unwrap_or_else(|_err| {
                 print_error(format!("Error saving chunk {}, {}", i, j));
@@ -77,7 +85,8 @@ fn print_error(message: impl std::fmt::Display) {
     eprintln!("{}", message);
 }
 
-fn find_last(str: &str, c: char) -> Option<usize> {
+fn find_last(str: impl std::fmt::Display, c: char) -> Option<usize> {
+    let str = str.to_string();
     let len = str.len();
     for (i, _c) in str.chars().rev().enumerate() {
         if c == _c {
